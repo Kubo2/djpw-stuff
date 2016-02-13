8d82ba8d54f30ac589b9507d4b9dbc3895d6617a
Kubis Fowler <Kubo2@users.noreply.github.com>
2016-02-13 17:25:38 +0530

    Create foo.js

---
+++ foo.js
@@ -0,0 +1,20 @@
+inits.push(function(param) {
+  if(!param.qstopic || 1) return;
+//  insertAnchor = function(el) {
+//    location.replace(el.innerHTML);
+//    var odkaz = "http://djpw.cz/" + param.qstopic + (parseInt(param.qspage) > 0 ? "/" + (parseInt(param.qspage) + 1) : "") + el.innerHTML;
+//    odkaz = "[url=" + odkaz + "]" + el.innerHTML + "[/url]";
+
+//    insertName(el, odkaz);
+//    // chyba neodscrollovania na textareu po volaní insertName()
+//    var t = document.forms['postMsg'] && document.forms['postMsg'].elements['poslText'];
+//    if(!t) {
+//      console && console.log("Nie je monžné aktivovať textareu (vloženie kotvy insertAnchor()).");
+//      return;
+//    }
+//    //t.focus();
+//    t.blur();
+//    //t.scrollIntoView && t.scrollIntoView();
+//    //t.focus();
+//  }
+});
