3125fc0f1992f4cede13a5566c065310cd094e0f
Kubis Fowler <Kubo2@users.noreply.github.com>
2016-02-13 17:30:48 +0530

    Update loader.js
    Correct the URL for loading raw files from GitHub

--- loader.js
+++ loader.js
@@ -8,8 +8,8 @@ writeStyle('djpw-purified');
 function formatHtml(name, type) {
   type = type.toLowerCase();
   switch(type) {
-    case 'js': return '<script src="https://github.com/Kubo2/djpw-stuff/blob/master/' + name + '.' + type + '"></script>';
-    case 'css': return '<style>@import "https://github.com/Kubo2/djpw-stuff/blob/master/' + name + '.' + type + '";</style>';
+    case 'js': return '<script src="https://raw.githubusercontent.com/Kubo2/djpw-stuff/master/' + name + '.' + type + '"></script>';
+    case 'css': return '<style>@import "https://raw.githubusercontent.com/Kubo2/djpw-stuff/master/' + name + '.' + type + '";</style>';
     default: throw new TypeError();
   }
 }
