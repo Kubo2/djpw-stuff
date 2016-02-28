8940bc6a44341ec8540511de2ecd886d1b082376
Kubis Fowler <Kubo2@users.noreply.github.com>
2016-02-28 18:05:56 +0530

    Make #nahled-tlacitko to :focus before #submitbtn

--- foo.js
+++ foo.js
@@ -1,20 +1,16 @@
-inits.push(function(param) {
-  if(!param.qstopic || 1) return;
-//  insertAnchor = function(el) {
-//    location.replace(el.innerHTML);
-//    var odkaz = "http://djpw.cz/" + param.qstopic + (parseInt(param.qspage) > 0 ? "/" + (parseInt(param.qspage) + 1) : "") + el.innerHTML;
-//    odkaz = "[url=" + odkaz + "]" + el.innerHTML + "[/url]";
 
-//    insertName(el, odkaz);
-//    // chyba neodscrollovania na textareu po volaní insertName()
-//    var t = document.forms['postMsg'] && document.forms['postMsg'].elements['poslText'];
-//    if(!t) {
-//      console && console.log("Nie je monžné aktivovať textareu (vloženie kotvy insertAnchor()).");
-//      return;
-//    }
-//    //t.focus();
-//    t.blur();
-//    //t.scrollIntoView && t.scrollIntoView();
-//    //t.focus();
-//  }
+/**
+ * Switch the [tabindex] of #nahled-tlacitko & #submitbtn
+ * to <kbd>Tab</kbd> be first focusing on the #nahled-tlacitko
+ */
+inits.push(function() {
+	var tmp, nahledTlactiko = document.getElementById('nahled-tlacitko'),
+		submitBtn = document.getElementById('submitbtn');
+	
+	if(nahledTlacitko && submitBtn) {
+		// kto to kedy sakra vymyslel, že [tabindex] sa v JS zapisuje .tabIndex
+		tmp = nahledTlacitko.tabIndex;
+		nahledTlacitko.tabIndex = submitBtn.tabIndex;
+		submitBtn.tabIndex = tmp;
+	}
 });
