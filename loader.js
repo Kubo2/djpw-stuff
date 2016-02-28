b717603f15f163d02e385c762e0728ec458d12b7
Kubis Fowler <Kubo2@users.noreply.github.com>
2016-02-28 18:15:37 +0530

    Only expose the window.Kubo2 interface

--- loader.js
+++ loader.js
@@ -1,27 +1,28 @@
+
+window.Kubo2 = window.Kubo2 || {};
+
 /**
  * Loads the extensions.
  */
+(function(k) {
+	function formatHtml(name, type) {
+		type = type.toLowerCase();
+		function getUrl() {
+			return 'https://rawgit.com/Kubo2/djpw-stuff/master/' + name + '.' + type;
+		}
 
-writeStyle('djpw-purified');
-//writeScript('foo');
-
-function formatHtml(name, type) {
-  type = type.toLowerCase();
-  function getUrl() {
-    return 'https://rawgit.com/Kubo2/djpw-stuff/master/' + name + '.' + type;
-  }
-  
-  switch(type) {
-    case 'js': return '<script src="' + getUrl() + '"></script>';
-    case 'css': return '<style>@import "' + getUrl() + '";</style>';
-    default: throw new TypeError();
-  }
-}
-
-function writeScript(name) {
-  document.write(formatHtml(name, 'js'));
-}
-
-function writeStyle(name) {
-  document.write(formatHtml(name, 'css'));
-}
+		switch(type) {
+			case 'js': return '<script src="' + getUrl() + '"></script>';
+			case 'css': return '<style>@import "' + getUrl() + '";</style>';
+			default: throw new TypeError();
+		}
+	}
+	
+	k.linkScript = function(name) {
+		document.write(formatHtml(name, 'js'));
+	};
+	
+	k.linkStyle = function(name) {
+		document.write(formatHtml(name, 'css'));
+	};
+})(window.Kubo2);
