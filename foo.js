9d2cc7ad99026b217780394a1a10de9c18ccd116
Kubis Fowler <Kubo2@users.noreply.github.com>
2016-04-29 01:57:11 +0530

    Add support for night style UI switch

--- foo.js
+++ foo.js
@@ -1,5 +1,24 @@
 
 /**
+ * Create a checkbox for #dark styles in header
+ */
+(function() {
+	var html = (function(){/*
+<label>
+	<input type="checkbox" onclick="document.documentElement.id = this.checked ? 'dark' : ''; document.cookies.write('night', +this.checked)">
+	Zhasnuté styly
+</label>
+	*/}).toString().slice(14, -3);
+
+	var parent = document.createElement('kubo2'),
+		menu = document.getElementById('menu');
+	parent.innerHTML = html;
+	parent.getElementsByTagName('input')[0].checked = (document.documentElement.id === 'dark');
+	menu.insertBefore(parent, menu.lastChild);
+})();
+
+
+/**
  * Switch the [tabindex] of #nahled-tlacitko & #submitbtn
  * to <kbd>Tab</kbd> be first focusing on the #nahled-tlacitko
  */
