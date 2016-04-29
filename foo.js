5d22009c7b8cbf8266929adc7202fa99ff7efd26
Kubis Fowler <Kubo2@users.noreply.github.com>
2016-04-29 15:29:17 +0530

    Nicely align the checkbox on the line

--- foo.js
+++ foo.js
@@ -5,7 +5,8 @@
 (function() {
 	var html = (function(){/*
 <label>
-	<input type="checkbox" onclick="document.documentElement.id = this.checked ? 'dark' : ''; document.cookies.write('night', +this.checked)">
+	<input type="checkbox" onclick="document.documentElement.id = this.checked ? 'dark' : ''; document.cookies.write('night', +this.checked)"
+	style='vertical-align: middle'>
 	Zhasnuté styly
 </label>
 	*/}).toString().slice(14, -3);
@@ -13,7 +14,7 @@
 	var parent = document.createElement('kubo2'),
 		menu = document.getElementById('menu');
 	parent.innerHTML = html;
-	parent.getElementsByTagName('input')[0].checked = (document.documentElement.id === 'dark');
+	parent.getElementsByTagName('input')[0].checked = (document.documentElement.id == 'dark');
 	menu.insertBefore(parent, menu.lastChild);
 })();
 
